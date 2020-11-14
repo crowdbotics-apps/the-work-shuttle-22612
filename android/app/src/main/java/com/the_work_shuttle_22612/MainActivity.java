eef65402cc4a4cc01a3e987742f33bb33d0bb924
Crowdbotics <team@crowdbotics.com>
2020-11-14 05:58:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/the_work_shuttle_22612/MainActivity.java
@@ -0,0 +1,15 @@
+package com.the_work_shuttle_22612;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "the_work_shuttle_22612";
+    }
+}
